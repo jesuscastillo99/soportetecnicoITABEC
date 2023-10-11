4b68687b13df50a9e21d46278b092eb0a183d571
Jesus <jesusalberto990@gmail.com>
2023-10-11 10:00:20 -0600

    antes de hacer el select en form1

--- app/Http/Controllers/Form1Controller.php
+++ app/Http/Controllers/Form1Controller.php
@@ -46,5 +46,37 @@ class Form1Controller extends Controller
         // Devuelve las localidades en formato JSON
         return response()->json($localidades);
     }
+
+    public function mostrarLocalidad(Request $request)
+    {
+        $usuario = Auth::user();
+        $idPersona = $usuario->id;
+        $idLocalidad = DB::table('personas')
+        ->where('id', $idPersona) // Reemplaza $personaId con el ID de la persona que deseas consultar
+        ->value('idlocalidad');
+
+        $localidad = DB::table('Catlocalidades')
+            ->select('Catlocalidades.Localidad', 'CatMunicipios.NombreMunicipio', 'CatEstado.NombreEstado')
+            ->join('CatMunicipios', 'Catlocalidades.IdMunicipio', '=', 'CatMunicipios.IdMunicipio')
+            ->join('CatEstados', 'CatMunicipios.IdEstado', '=', 'CatEstados.IdEstado')
+            ->where('Catlocalidades.IdLocalidad', $idLocalidad)
+            ->first();
+
+        if ($localidad) {
+            $nombreLocalidad = $localidad->Localidad;
+            $nombreMunicipio = $localidad->NombreMunicipio;
+            $nombreEstado = $localidad->NombreEstado;
+
+            // Pasar las variables a la vista
+            return view('layouts-form.form1', [
+                'nombreLocalidad' => $nombreLocalidad,
+                'nombreMunicipio' => $nombreMunicipio,
+                'nombreEstado' => $nombreEstado
+            ]);
+        } else {
+            // Manejar el caso donde no se encontró la localidad.
+            return view('layouts-form.form1')->with('error', 'La localidad no fue encontrada');
+        }
+    }
     
 }
